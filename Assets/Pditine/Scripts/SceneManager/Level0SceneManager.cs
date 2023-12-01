3b4076a0e43a8a14902c2448f245e1e1e2478863
Pditine <hanfe666@outlook.com>
2023-12-01 13:28:18 +0800

    Level0 is done

---
+++ Assets/Pditine/Scripts/SceneManager/Level0SceneManager.cs
@@ -0,0 +1,26 @@
+﻿using Pditine.Scripts.Tool;
+using UnityEngine;
+using UnityEngine.Serialization;
+using UnityEngine.UI;
+
+namespace Pditine.Scripts.SceneManager
+{
+    public class Level0SceneManager : MonoBehaviour
+    {
+        [SerializeField] private Image logUI;
+        [SerializeField] private Image playUI;
+        [SerializeField] private Image exitUI;
+
+        public void Play()
+        {
+            FadeUtility.FadeInAndStay(logUI,60);
+            FadeUtility.FadeOut(playUI,100);
+            FadeUtility.FadeOut(exitUI,100);
+        }
+
+        public void Exit()
+        {
+            Application.Quit();
+        }
+    }
+}
