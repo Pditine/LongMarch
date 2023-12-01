6e497627ea2ec0f524c1b6b3be1ff7832e55c558
Pditine <hanfe666@outlook.com>
2023-12-01 15:42:34 +0800

    repair level 0

--- Assets/Pditine/Scripts/SceneManager/Level1SceneManager.cs
+++ Assets/Pditine/Scripts/SceneManager/Level1SceneManager.cs
@@ -1,4 +1,6 @@
-﻿using Pditine.Scripts.Tool;
+﻿using Hmxs.Scripts.Protagonist;
+using Hmxs.Toolkit.Flow.FungusTools;
+using Pditine.Scripts.Tool;
 using UnityEngine;
 using UnityEngine.Serialization;
 using UnityEngine.UI;
@@ -10,12 +12,20 @@ namespace Pditine.Scripts.SceneManager
         [SerializeField] private Image logUI;
         [SerializeField] private Image playUI;
         [SerializeField] private Image exitUI;
+        [SerializeField] private ProtagonistController player;
 
         public void Play()
         {
             FadeUtility.FadeInAndStay(logUI,60);
             FadeUtility.FadeOut(playUI,100);
             FadeUtility.FadeOut(exitUI,100);
+            Invoke(nameof(PlayerCanMove),2.5f);
+            FlowchartManager.ExecuteBlock("GameStart");
+        }
+
+        private void PlayerCanMove()
+        {
+            player.enabled = true;
         }
 
         public void Exit()
