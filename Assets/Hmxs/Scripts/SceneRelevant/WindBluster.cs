577069de8c681c28ba300d7898cfa81c1d2ce027
Hmxs <18321938171@163.com>
2023-12-02 20:47:15 +0800

    complete a part of snow scene

---
+++ Assets/Hmxs/Scripts/SceneRelevant/WindBluster.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using UnityEngine;
+
+namespace Hmxs.Scripts.SceneRelevant
+{
+    public class WindBluster : MonoBehaviour
+    {
+        public Rigidbody2D protagonistRb;
+
+        public float windIntensity;
+
+        private void FixedUpdate()
+        {
+            protagonistRb.velocity -= new Vector2(windIntensity, 0);
+        }
+    }
+}
