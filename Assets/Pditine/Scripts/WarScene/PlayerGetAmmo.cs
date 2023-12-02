658be008389b07e7334c2ee86acc84f7f08b1f98
Pditine <hanfe666@outlook.com>
2023-12-02 20:11:31 +0800

    add GetAmmo.cs

---
+++ Assets/Pditine/Scripts/WarScene/PlayerGetAmmo.cs
@@ -0,0 +1,25 @@
+﻿using Pditine.Scripts.Tool;
+using UnityEngine;
+
+namespace Pditine.Scripts.WarScene
+{
+    public class PlayerGetAmmo : MonoSingleton<PlayerGetAmmo>
+    {
+        private bool _hasAmmo;
+        private GameObject _ammoIcon;
+        
+        private void GetAmmo()
+        {
+            _hasAmmo = true;
+            _ammoIcon.SetActive(true);
+        }
+
+        private bool CheckAndGiveAmmo()
+        {
+            var hasAmmo = _hasAmmo;
+            _hasAmmo = false;
+            _ammoIcon.SetActive(false);
+            return _hasAmmo;
+        }
+    }
+}
