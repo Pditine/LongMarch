222537466f6b4faffe898d48aa3255807032c2a0
Hmxs <18321938171@163.com>
2023-12-03 00:36:02 +0800

    Update Snow Scene

---
+++ Assets/Hmxs/Scripts/Protagonist/SnowSceneProtagonistController.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+
+namespace Hmxs.Scripts.Protagonist
+{
+    public class SnowSceneProtagonistController : MonoBehaviour
+    {
+        public float speed;
+        public float windIntensity;
+
+        private float _movementInput;
+
+        private Rigidbody2D  _rigidbody;
+        private Animator _animator;
+
+        private static readonly int IsWalking = Animator.StringToHash("IsWalking");
+
+        private void Start()
+        {
+            _rigidbody = GetComponent<Rigidbody2D>();
+            _animator = GetComponent<Animator>();
+        }
+
+        private void Update()
+        {
+            InputHandler();
+            UpdateAnimState();
+        }
+
+        private void FixedUpdate()
+        {
+            ApplyMovement();
+        }
+
+        private void InputHandler()
+        {
+            _movementInput = Input.GetAxis("Horizontal");
+        }
+
+        private void ApplyMovement()
+        {
+            _rigidbody.velocity = new Vector2(_movementInput * speed - windIntensity, _rigidbody.velocity.y);
+        }
+
+        private void UpdateAnimState()
+        {
+            _animator.SetBool(IsWalking, Mathf.Abs(_rigidbody.velocity.x) > 0.01);
+        }
+    }
+}
---
+++ Assets/Hmxs/Scripts/SnowMountainManager.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using Cinemachine;
+using UnityEngine;
+
+namespace Hmxs.Scripts
+{
+    public class SnowMountainManager : MonoBehaviour
+    {
+        public List<Transform> startPoints;
+        public List<PolygonCollider2D> confiners;
+
+        public CinemachineConfiner2D confiner;
+        public Transform protagonist;
+        public Transform camera;
+
+        public void DistrictTransit(int districtNumber)
+        {
+            protagonist.position = startPoints[districtNumber].position;
+            camera.position = startPoints[districtNumber].position;
+            confiner.m_BoundingShape2D = confiners[districtNumber];
+        }
+    }
+}
