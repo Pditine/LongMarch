4fcf476269c8ea6d7773817271838c8325810faa
Pditine <hanfe666@outlook.com>
2023-12-03 14:28:34 +0800

    build level6

---
+++ Assets/Pditine/Scripts/LevelSceneManager/Level6SceneManager.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+using Pditine.Scripts.Item;
+using UnityEngine;
+
+namespace Pditine.Scripts.LevelSceneManager
+{
+    public class Level6SceneManager : MonoBehaviour
+    {
+        [SerializeField] private List<SoldierOnTheirStomachs> soldiers = new();
+
+        private void Start()
+        {
+            foreach (var soldier in soldiers)
+            {
+                soldier.ChangeBulletCount(200);
+            }
+        }
+    }
+}
